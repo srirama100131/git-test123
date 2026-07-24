bbc6d01173dc8452f19fefa983f19d7361659ce9
rajubandi <rajubandi@gmail.com>
2026-07-24 20:20:04 +0530

    added subnet in vnet block

--- vnet.tf
+++ vnet.tf
@@ -4,3 +4,10 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
 }
+
+resource "azurerm_subnet" "subnet1" {
+  name = "dev-subnet1"
+  address_prefixes = ["10.1.1.0/24"]
+  resource_group_name = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+}
